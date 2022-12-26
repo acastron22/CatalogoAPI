fb429383ac5bb0f0ce4d441da4285364aed58d64
acastron22 <arielcastron22@hotmail.com>
2022-12-26 18:13:16 -0300

    primeiro comit

---
+++ CatalagoAPI/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CatalagoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalagoAPI.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+
+
+}
---
+++ CatalagoAPI/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace CatalagoAPI.Models;
+
+public class Categoria
+{
+    public int CategoriaId{ get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+    public ICollection<Produto>? Produtos{ get; set; }
+
+
+}
