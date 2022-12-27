227d05780d6bc8a3c11f1c01b3373e5f1a304ea7
acastron22 <arielcastron22@hotmail.com>
2022-12-26 23:38:02 -0300

    Criando a tabela com migrations

--- CatalagoAPI/Context/AppDbContext.cs
+++ CatalagoAPI/Context/AppDbContext.cs
@@ -11,4 +11,5 @@ public class AppDbContext : DbContext
     public DbSet<Produto>? Produtos { get; set; }
 
 
+
 }
--- CatalagoAPI/Models/Categoria.cs
+++ CatalagoAPI/Models/Categoria.cs
@@ -1,7 +1,13 @@
-﻿namespace CatalagoAPI.Models;
+﻿using System.Collections.ObjectModel;
+
+namespace CatalagoAPI.Models;
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId{ get; set; }
     public string? Nome { get; set; }
     public string? ImageUrl { get; set; }
