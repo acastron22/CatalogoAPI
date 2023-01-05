504a2ef11343ddd01977162dbc0c15263b7e80dc
Ariel Castro Neves <ariel.neves@planejamento.rj.gov.br.local>
2023-01-05 11:41:41 -0300

    validação parte 2

--- CatalagoAPI/Models/Produto.cs
+++ CatalagoAPI/Models/Produto.cs
@@ -7,14 +7,14 @@ namespace CatalagoAPI.Models;
 
 [Table("Produtos")]
 
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoID { get; set; }
 
     [Required(ErrorMessage = "O nome é obrigatório")]
     [StringLength(20, ErrorMessage = "O nome deve ter entre 5 e 20 caracteres", MinimumLength = 5)]
-    [PrimeiraLetraMaiuscula]
+    //[PrimeiraLetraMaiuscula]
     public string? Name{ get; set; }
 
     [Required]
@@ -42,4 +42,30 @@ public class Produto
     [JsonIgnore]
     public Categoria? Categoria{ get; set; }
 
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Name))
+        {
+            var primeiraLetra = this.Name[0].ToString();
+
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra do produto deve ser maiúscula",
+                    new[]
+                    { nameof(this.Name) }
+                    );
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new
+                    ValidationResult("O estoque deve ser maior que 0",
+                    new[]
+                    {
+                        nameof(this.Estoque) }
+                    );
+            }
+        }
+    }
 }
